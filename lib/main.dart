8055aa29036d8fa29533c2d492dbcc184996f1e1
Hazem Hamdy7 <hazemhamdy1215@gmail.com>
2024-03-26 12:41:40 -0700

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(BirthdayCard());
+  runApp(const BirthdayCard());
 }
 
 class BirthdayCard extends StatelessWidget {
+  const BirthdayCard({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Color(0xFFD2BCD5),
         body: Center(
